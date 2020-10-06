8f521a8b858e5aa6263423ff9ad487a4a19a3973
Millefiore <Crawlette@gmail.com>
2020-10-06 20:30:39 +0300

    Updates

---
+++ precache-manifest.c69f10370730488c17c8d432f33c0787.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293e10803637aefe7e8829c43f5d300d",
+    "url": "/Millefiore.github.io/index.html"
+  },
+  {
+    "revision": "27174bc80a3023505208",
+    "url": "/Millefiore.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e69c9369d8a6b0ae42a3",
+    "url": "/Millefiore.github.io/static/js/2.9aefffc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Millefiore.github.io/static/js/2.9aefffc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27174bc80a3023505208",
+    "url": "/Millefiore.github.io/static/js/main.c11ca465.chunk.js"
+  },
+  {
+    "revision": "61554a715f3b3e88bc4f",
+    "url": "/Millefiore.github.io/static/js/runtime-main.7ca4ce8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Millefiore.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
